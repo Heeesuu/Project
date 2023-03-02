a4ca90ff314ac962d1c4f4ba482cb6eb567db94a
Heeesuu <gmltn0508@icloud.com>
2023-03-02 09:28:30 +0900

    4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int id = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("== 명언 앱 ==");
+        while (true) {
+            System.out.printf("명령 ) ");
+            String cmd = sc.nextLine();
+
+            if (cmd.equals("등록")) {
+                System.out.printf("명언 : ");
+                String content = sc.nextLine();
+                System.out.print("작가 : ");
+                String author = sc.nextLine();
+                id++;
+                System.out.println(id + "번 명언이 등록되었습니다.");
+
+            }
+            if (cmd.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("-----------------------");
+            }
+
+            if (cmd.equals("종료")) {
+                break;
+            }
+        }
+        sc.close();
+
     }
 }
