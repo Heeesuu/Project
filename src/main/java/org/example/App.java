5cf2480aecf335a07e89e798c9107f67ce9b2d55
Heeesuu <gmltn0508@icloud.com>
2023-03-02 15:29:57 +0900

    6단계_1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,9 @@ package org.example;
 import org.example.system.controller.SystemController;
 import org.example.wiseSaying.controller.WiseSayingController;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class App {
@@ -30,8 +33,32 @@ public class App {
 
             } else if (command.equals("목록")) {
                 WiseSayingController.list();
+            } else if (command.startsWith("삭제")){
+                //정리시작
+
+                String[] commandBits = command.split("\\?",2); // ?를 기준으로 최대 두개까지 나눈다.
+                String actionCode = commandBits[0];
+
+                Map<String, String> params = new HashMap<>();
+
+                String[] paramBits = commandBits[1].split("&");
+
+                for (String paramStr : paramBits) {
+                    String[] paramStrBits = paramStr.split("=", 2);
+                    String key = paramStrBits[0];
+                    String value = paramStrBits[1];
+
+                    params.put(key, value);
                 }
+
+                System.out.printf("actionCode : %s\n", actionCode);
+                System.out.printf("params : %s\n", params);
+
+                //정리끝
             }
+
+
         }
     }
+}
 
