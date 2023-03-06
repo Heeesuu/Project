0af18ee0a95ab8c2738a2e25afe036de861d9108
Heeesuu <gmltn0508@icloud.com>
2023-03-06 16:40:51 +0900

    4단계

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -4,12 +4,17 @@ import java.util.Scanner;
 
 public class Application {
     public final Scanner sc;
+    ;
+
     public Application(Scanner sc){
         this.sc = sc;
     }
 
     public void run() {
         System.out.println("== 명언 앱 ==");
+
+        int lastId = 1;
+
         while (true) {
             System.out.print("명령 )");
             String command = sc.nextLine().trim();
@@ -21,6 +26,9 @@ public class Application {
                 String content = sc.nextLine().trim();
                 System.out.print("작가 : ");
                 String authorName = sc.nextLine().trim();
+
+                System.out.printf("%d번 명언이 등록되었습니다.\n", lastId);
+                lastId++;
 
             }
         }
