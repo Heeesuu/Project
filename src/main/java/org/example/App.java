3ab7d3a66dd7fc7001684f048329291c432c3f99
Heeesuu <gmltn0508@icloud.com>
2023-03-06 17:23:51 +0900

    8단계까지 완성

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import org.example.system.controller.SystemController;
+import org.example.wiseSaying.controller.WiseSayingController;
+
+public class App {
+    public void run() {
+
+        SystemController systemController = new SystemController();
+        WiseSayingController wiseSayingController = new WiseSayingController();
+
+
+        System.out.println("== 명언 앱 ==");
+        while (true) {
+            System.out.print("명령 ) ");
+            String command = Container.getScanner().nextLine().trim();
+            Rq rq = new Rq(command);
+
+            switch (rq.getActionCode()) {
+                case "종료" :
+                    SystemController.exit();
+                    return;
+
+                case "등록" :
+                    wiseSayingController.write();
+                    break;
+
+                case "목록" :
+                    wiseSayingController.list();
+                    break;
+
+                case "삭제" :
+                    wiseSayingController.remove(rq);
+
+
+                case "수정" :
+                    wiseSayingController.modify(rq);
+
+            }
+        }
+
+
+    }
+}
